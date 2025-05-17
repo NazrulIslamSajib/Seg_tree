5ca099f3b9d05f0076d41dd004ff7f7d6d499253
Nazrul Islam Sajib <124718648+NazrulIslamSajib@users.noreply.github.com>
2025-05-17 10:07:43 +0600

    Create dbg.cpp

---
+++ Debuggin/dbg.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Gene template< class
+#define Rics printer& operator,
+Gene c> struct rge{c b, e;};
+Gene c> rge<c> range(c i, c j){ return {i, j};}
+struct printer{
+    ~printer(){cerr<<endl;}
+    Gene c >Rics(c x){ cerr<<boolalpha<<x; return *this;}
+    Rics(string x){cerr<<x;return *this;}
+    Gene c, class d >Rics(pair<c, d> x){ return *this,"(",x.first,", ",x.second,")";}
+    Gene ... d, Gene ...> class c >Rics(c<d...> x){ return *this, range(begin(x), end(x));}
+    Gene c >Rics(rge<c> x){
+        *this,"["; for(auto it = x.b; it != x.e; ++it)
+            *this,(it==x.b?"":", "),*it; return *this,"]";}
+};
+#define debug() cerr<<"LINE "<<__LINE__<<" >> ", printer()
+#define dbg(x) "[",#x,": ",(x),"] "
+#define tham getchar()
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long tt;
+    cin>>tt;
+    while(tt--)
+    {
+         int x=5;
+         debug(),dbg(x);
+         int y=5;
+         debug(),dbg(y);
+    }
+
+    return 0;
+}
+
+
+
